e75484e59bfa688f12420e2e75a80b1182a2835f
irfan <85334710+irfan-007@users.noreply.github.com>
2024-08-01 14:47:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function insert_Row() {
-    //Write your code here
-  let table=document.getElementById("sampleTable");
-	let tr=document.createElement("tr");
-	let td1=document.createElement("td");
-	let td2=document.createElement("td");
-	td1.innerText="New Cell1";
-	td2.innerText="New Cell2";
-	tr.append(td1,td2);
-	table.insertBefore(tr,table.childNodes[0]);
-
-  
+  //Write your code here
+  let table = document.querySelector("#sampleTable tbody");
+  let tr = document.createElement("tr");
+  let td1 = document.createElement("td");
+  let td2 = document.createElement("td");
+  td1.innerText = "New Cell1";
+  td2.innerText = "New Cell2";
+  tr.append(td1, td2);
+  table.insertBefore(tr, table.childNodes[0]);
 }
+
