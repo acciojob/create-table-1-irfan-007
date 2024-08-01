f97eb5dfd62841477eb240819c9905142b6907e0
irfan <85334710+irfan-007@users.noreply.github.com>
2024-08-01 11:39:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function insert_Row() {
     //Write your code here
-  
+  let table=document.getElementById("sampleTable");
+	let tr=document.createElement("tr");
+	tr.innerHTML="<td>New Cell1</td><td>New Cell2</td>";
+	table.prepend(tr);
+
   
 }
